6b9523f76d1e33ecf1e2c702979fbf1e408610c7
lucasferreiralimax <lucasferreiralimax@gmail.com>
2020-11-17 21:03:55 -0300

    adjustment page login

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -1,24 +1,41 @@
 <template>
   <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
-    <div>
-      <div style="font-size: 30vh">
+    <q-card class="q-pa-lg">
+      <h4 class="q-mt-none q-mb-lg">
         Login
-      </div>
+      </h4>
+      <q-input class="q-mb-md" outlined color="black" bg-color="white" v-model="user" :label="$t('login.user')">
+        <template v-slot:prepend>
+          <q-icon name="person" color="black" />
+        </template>
+      </q-input>
+      <q-input outlined color="black" bg-color="white" v-model="pass" :label="$t('login.pass')">
+        <template v-slot:prepend>
+          <q-icon name="lock" color="black" />
+        </template>
+      </q-input>
       <q-btn
-        class="q-mt-xl"
+        class="q-mt-lg full-width"
         color="white"
         text-color="blue"
         unelevated
+        size="lg"
         to="/"
-        label="Go Home"
+        :label="$t('login.enter')"
         no-caps
       />
-    </div>
+    </q-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Error404'
+  name: 'Login',
+  data () {
+    return {
+      user: '',
+      pass: ''
+    }
+  }
 }
 </script>
