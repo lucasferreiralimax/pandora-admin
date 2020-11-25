6f3c45b770d2be863b988206b2b4993d4de2a9ae
lucasferreiralimax <lucasferreiralimax@gmail.com>
2020-11-25 10:56:55 -0300

    adjustment color page 404

--- src/pages/Error404.vue
+++ src/pages/Error404.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="fullscreen bg-blue text-white text-center q-pa-md flex flex-center">
+  <div class="fullscreen bg-grey text-white text-center q-pa-md flex flex-center">
     <div>
       <div style="font-size: 30vh">
         404
@@ -12,10 +12,10 @@
       <q-btn
         class="q-mt-xl"
         color="white"
-        text-color="blue"
+        text-color="green"
         unelevated
         to="/"
-        label="Go Home"
+        label="Voltar para Home"
         no-caps
       />
     </div>
